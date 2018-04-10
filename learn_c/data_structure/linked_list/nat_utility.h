6e388972d836182628a32c1b79fde27072a98868
Yung Coder <37970614+nguyenatan@users.noreply.github.com>
2018-04-10 13:08:55 +0700

    Create nat_utility.h

---
+++ learn_c/data_structure/linked_list/nat_utility.h
@@ -0,0 +1,12 @@
+#ifndef _NAT_UTILITY_H
+#define _NAT_UTILITY_H
+
+typedef enum { false, true } bool;
+
+#define offsetof(type, member) (size_t)&(((type *)0)->member)
+
+#define container_of(ptr, type, member) ({			\
+	const typeof( (type *)0)->member ) *__mptr = (ptr);	\
+	(type *)( (char *)__mptr - offsetof(type, member) );})
+
+#endif
