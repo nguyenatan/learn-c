7b218aac1e93c72e93ef1235507d96dd6aad0151
A Tan <37970614+nguyenatan@users.noreply.github.com>
2018-04-23 05:29:23 +0700

    Update nat_utility.h

--- learn_c/data_structure/linked_list/nat_utility.h
+++ learn_c/data_structure/linked_list/nat_utility.h
@@ -6,7 +6,7 @@ typedef enum { false, true } bool;
 #define offsetof(type, member) (size_t)&(((type *)0)->member)
 
 #define container_of(ptr, type, member) ({			\
-	const typeof( (type *)0)->member ) *__mptr = (ptr);	\
+	const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
 	(type *)( (char *)__mptr - offsetof(type, member) );})
 
 #endif
