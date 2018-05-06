bab62b0def99b26fd4d5da92983dcd08037598dc
A$AP A Tvn <37970614+nguyenatan@users.noreply.github.com>
2018-05-06 19:09:53 +0700

    Update show_bits.c

--- learn_c/algorithm/bitwise/show_bits.c
+++ learn_c/algorithm/bitwise/show_bits.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
     
 void show_bit(unsigned int x)
-{
+{	
 	int i = sizeof(int) * 8 - 1;
 	for(; i >= 0; --i) {
 		
-		if ( !((i + 1) % 8) )
+		if (i != 31 && !((i + 1) % 4))
 			putchar(' ');
 		
 		(x & (1u << i)) ? putchar('1') : putchar('0');
@@ -16,7 +16,7 @@ void show_bit(unsigned int x)
 
 int main() 
 {
-	printf("47 in binary");
+	printf("47 in binary ");
 	show_bit(47);
 	
 	return 0;
