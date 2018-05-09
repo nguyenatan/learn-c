4d74a5966a8846f879466424f33534e08dbc9a1f
A Tan <37970614+nguyenatan@users.noreply.github.com>
2018-05-09 09:52:16 +0700

    Create find_the_number_occurring_odd_number_of_times.c

---
+++ learn_c/algorithm/bitwise/basic/find_the_number_occurring_odd_number_of_times.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+ * x ^ 0 = x
+ * x ^ y = 0
+ * x ^ x ^ y = y
+ */
+int get_odd_occurrence(int *array, unsigned size)
+{
+	int res = 0;
+	
+	unsigned i = 0;
+	for (; i < size; ++i)
+		res ^= array[i];
+	
+	return res;
+		
+}
+
+int main(void)
+{
+	int array[] = {0, 3, 2, 2, 0, 3, 2, 2, 0};
+	unsigned size = *(&array+1) - array;
+	
+	printf("%d\n", get_odd_occurrence(array, size));
+	
+	return 0;
+}
