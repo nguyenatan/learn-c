3f63a289353e394285ab3e33ea9f079816e13276
A Tan <37970614+nguyenatan@users.noreply.github.com>
2018-05-15 04:25:04 +0700

    Update count_set_bits.c

--- learn_c/algorithm/bitwise/basic/count_set_bits.c
+++ learn_c/algorithm/bitwise/basic/count_set_bits.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
+static const unsigned char bits_set_table_256[256] =
+{
+	#define B2(n) 	n,	n+1,	n+1,	n+2
+	#define B4(n)	B2(n),	B2(n+1),B2(n+1),B2(n+2)
+	#define B6(n)	B4(n),	B4(n+1),B4(n+1),B4(n+2)
+	B6(0), B6(1), B6(1), B6(2)
+};
+
 unsigned count_set_bit(unsigned value)
 {
-	unsigned count = 0;
-	
-	while (value) {
-		value &= (value - 1);
-		++count;
-	}
-	
-	return count;
+	return bits_set_table_256[value & 0xFF] +
+	       bits_set_table_256[(value >> 8) & 0xFF] +
+	       bits_set_table_256[(value >> 16) & 0xFF] +
+	       bits_set_table_256[(value >> 24) & 0xFF];
 }
 
 int main(void)
