3dbfd4328b7b2b2bc47a68b9b2473057ea2908f5
A Tan <37970614+nguyenatan@users.noreply.github.com>
2018-05-15 02:47:12 +0700

    Create check_if_a_number_is_a_power_of_four_or_not.c

---
+++ learn_c/algorithm/bitwise/basic/check_if_a_number_is_a_power_of_four_or_not.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define bool int
+
+bool is_power_of_four(unsigned value)
+{
+	unsigned count = 0;
+	
+	if ( value && !(value & (value - 1)) ) {
+		
+		while (value > 1) {
+			value >>= 1;
+			++count;
+		}
+	}
+	
+	return !(count % 2) ? 1 : 0;
+}
+
+int main(void)
+{
+	printf("%d\n", is_power_of_four(1)); // true
+	
+	return 0;
+}
