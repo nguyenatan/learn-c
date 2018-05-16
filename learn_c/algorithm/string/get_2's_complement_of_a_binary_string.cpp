081bd2b46ebdc533a6e4ed0aaf5309f0a7e69777
A Tan <37970614+nguyenatan@users.noreply.github.com>
2018-05-16 17:57:32 +0700

    Create get_2's_complement_of_a_binary_string.cpp

---
+++ learn_c/algorithm/string/get_2's_complement_of_a_binary_string.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string to_two_complement(string str)
+{
+	int i = str.length();
+	for (--i; i >= 0; --i)
+		if (str[i] == '1')
+			break;
+	
+	if (i == -1)
+		return '1' + str;
+	
+	for (--i; i >= 0; --i) {
+		
+		if (str[i] == '1')
+			str[i] = '0';
+		else
+			str[i] = '1';
+	}
+	
+	return str;
+}
+
+int main()
+{
+	string str = "00101111";
+	
+	cout << to_two_complement(str) << "\n";
+	
+	return 0;
+}
