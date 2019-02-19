981dafd3a35d96774c5874ed65293bd5c6a194e3
nat <37970614+nguyenatan@users.noreply.github.com>
2019-02-19 17:36:40 +0700

    Create strlen.c

---
+++ learn_c/string/strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+size_t strlen(const char *s)
+{
+    size_t i;
+    for (i = 0; s[i]; ++i) ;
+    return i;
+}
+
+int main(void)
+{
+    char *str = "Nguyen A Tan";
+    printf("%d\n", strlen(str)); // 12
+
+    return 0;
+}
